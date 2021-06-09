d02cb07b77eb89c156bd9262b9951cd875fd1f30
Juliant Daniel <daniel.latuary@gmail.com>
2021-06-09 23:20:54 +0700

    operational

--- app/Http/Controllers/OperationalController.php
+++ app/Http/Controllers/OperationalController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Operational;
 use Illuminate\Http\Request;
+
 
 class OperationalController extends Controller
 {
-    //
+
+    public function index()
+    {
+        return Operational::all();
+    }
+
+    public function create(request $request)
+    {
+        // $Operational = new Operational;
+        // $Operational->quantity_exp = $request->quantity_exp;
+        // $Operational->date = $request->date;
+        // $Operational->save();
+
+        // return "Data Berhasil Masuk";
+    }
+
+    public function update(Request $request, $id_Operational)
+    {
+        // $Operational = Operational::findOrFail($id_Operational);
+        // $Operational->update($request->all());
+
+        // return "Data Berhasil di Update";
+    }
+
+    public function delete($id_Operational)
+    {
+        $Operational = Operational::find($id_Operational);
+        $Operational->delete();
+
+        return "Data Berhasil di Hapus";
+    }
 }
--- app/Models/operational.php
+++ app/Models/operational.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class operational extends Model
 {
     use HasFactory;
+    protected $table = "operationals";
 }
