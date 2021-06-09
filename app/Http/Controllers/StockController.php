8d8d7d7bef38c893210f024f0a323bd3c2632d49
yusufyop <rahmatsyahpp@gmail.com>
2021-06-09 21:08:27 +0700

    no message

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\stock;
 use Illuminate\Http\Request;
+
 
 class StockController extends Controller
 {
-    //
+
+    public function index()
+    {
+        return stock::all();
+    }
+
+    public function create(request $request)
+    {
+        $stock = new Stock;
+        $stock->location = $request->location;
+        $stock->capacity = $request->capacity;
+        $stock->save();
+
+        return "Data Berhasil Masuk";
+    }
+
+    public function update(Request $request, $stock_id)
+    {
+        $stock = stock::findOrFail($stock_id);
+        $stock->update($request->all());
+
+        return "Data Berhasil di Update";
+    }
+
+    public function delete($stock_id)
+    {
+        $stock = stock::find($stock_id);
+        $stock->delete();
+
+        return "Data Berhasil di Hapus";
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,27 @@ Route::post('/inbound', 'App\Http\Controllers\InboundController@create');
 Route::put('/inbound/{id_inbound}', 'App\Http\Controllers\InboundController@update');
 Route::delete('/inbound/{id_inbound}', 'App\Http\Controllers\InboundController@delete');
 
+Route::get('/outbound', 'App\Http\Controllers\OutboundController@index');
+Route::post('/outbound', 'App\Http\Controllers\OutboundController@create');
+Route::put('/outbound/{id_outbound}', 'App\Http\Controllers\OutboundController@update');
+Route::delete('/outbound/{id_outbound}', 'App\Http\Controllers\OutboundController@delete');
+
+Route::get('/maintenance', 'App\Http\Controllers\MaintenanceController@index');
+Route::post('/maintenance', 'App\Http\Controllers\MaintenanceController@create');
+Route::put('/maintenance/{id_inbound}', 'App\Http\Controllers\MaintenanceController@update');
+Route::delete('/maintenance/{id_Maintenance}', 'App\Http\Controllers\MaintenanceController@delete');
+
+Route::get('/stock', 'App\Http\Controllers\StockController@index');
+Route::post('/stock', 'App\Http\Controllers\StockController@create');
+Route::put('/stock/{id_stock}', 'App\Http\Controllers\StockController@update');
+Route::delete('/stock/{id_stock}', 'App\Http\Controllers\StockController@delete');
+
+Route::get('/warehouse', 'App\Http\Controllers\WarehouseController@index');
+Route::post('/warehouse', 'App\Http\Controllers\WarehouseController@create');
+Route::put('/warehouse/{id_warehouse}', 'App\Http\Controllers\WarehouseController@update');
+Route::delete('/warehouse/{id_warehouse}', 'App\Http\Controllers\WarehouseController@delete');
+
+Route::get('/operational', 'App\Http\Controllers\OperationalController@index');
+Route::post('/operational', 'App\Http\Controllers\OperationalController@create');
+Route::put('/operational/{id_operational}', 'App\Http\Controllers\OperationalController@update');
+Route::delete('/operational/{id_operational}', 'App\Http\Controllers\OperationalController@delete');
